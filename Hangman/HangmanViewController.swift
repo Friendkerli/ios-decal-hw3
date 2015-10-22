315b98cfe229641b7c9526f619a9e122638b50d0
Friendkerli <yangli@MacBook-Pro.local>
2015-10-21 19:12:02 -0700

    first

--- Hangman/HangmanViewController.swift
+++ Hangman/HangmanViewController.swift
@@ -9,15 +9,29 @@
 import UIKit
 
 class HangmanViewController: UIViewController {
+    
+    @IBOutlet weak var guess: UIButton!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var currentText: UILabel!
+    @IBOutlet weak var image: UIImageView!
+    
+    var game = Hangman()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        game.start()
+        currentText.text = game.knownString
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func guessButtonPressed(sender: UIButton) {
+        textField.text = textField.text!.uppercaseString
+        currentText.text = textField.text
     }
 }
 
